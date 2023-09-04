fe05841b9418d44318d33884cf89957f563f81f6
Anil Vishwakarma <79105232+IamAnilVishwakarma@users.noreply.github.com>
2023-09-04 09:08:52 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as newer version.
+
+// alert("hello");   // we are using node js not browser.
+
+console.log(tyesof undefind) // undefind 
+console.log(tyesof null)   // object
